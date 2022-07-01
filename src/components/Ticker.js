f11065a12f8266525fd9553c7366e55766244af8
anguianoo <njanguiano@gmail.com>
2022-06-30 18:37:36 -0700

    done

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,23 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { makeRandomNumber } from "../utils";
+import React, { useEffect, useState, useRef } from "react"
+import { makeRandomNumber } from "../utils"
 
 function Ticker() {
-  const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+  const [price, setPrice] = useState(0)
+  const [color, setColor] = useState("black")
+
+  const prevPriceRef = useRef(price)
 
   useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
+    // we need some way to get the prevPrice...
+    const prevPrice = prevPriceRef.current
+    if (price > prevPrice) {
+      setColor("green")
+    } else if (price < prevPrice) {
+      setColor("red")
+    } else {
+      setColor("black")
+    }
+    // set the new value of the ref (note: this doesn't trigger a re-render)
+    prevPriceRef.current = price
+  }, [price])
+
+  useEffect(() => {
+    const id = setInterval(() => setPrice(makeRandomNumber), 1000)
     return function () {
-      clearInterval(id);
-    };
-  }, []);
+      clearInterval(id)
+    }
+  }, [])
 
   return (
     <div>
       <h1>TickerMaster</h1>
       <h2 style={{ color: color }}>Price: ${price}</h2>
     </div>
-  );
+  )
 }
 
-export default Ticker;
+export default Ticker
